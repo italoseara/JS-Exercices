5f8f11b9d568d57b709de9ed9216e9025b03b451
Italo Seara <54297273+italoseara@users.noreply.github.com>
2020-09-15 13:17:53 -0300

    Update 05_2.js

--- 05_2.js
+++ 05_2.js
@@ -2,58 +2,55 @@
 var John = {
     tip: [],
     payments: [],
-    tipCalc: function tipCalc(bill) {
-        if (bill < 50) {
-            var tip = 20/100;
-        } else if (bill <= 50 && bill <= 200) {
-            var tip = 15/100;
-        } else {
-            var tip = 10/100;
+    tipCalc: function () {
+        for (i = 0; i < this.bill.length; i++) {
+            if (this.bill[i] < 50) {
+                var tip = 20/100;
+            } else if (this.bill[i] <= 50 && this.bill[i] <= 200) {
+                var tip = 15/100;
+            } else {
+                var tip = 10/100;
+            }
+            this.tip.push(this.bill[i] * tip);
+            this.payments.push((this.bill[i] * tip) + this.bill[i]);
         }
-        this.tip.push(bill * tip);
-        this.payments.push((bill * tip) + bill);
     }
 }
 
 var Mark = {
     tip: [],
     payments: [],
-    tipCalc: function tipCalc(bill) {
-        if (bill < 100) {
-            var tip = 20/100;
-        } else if (bill <= 100 && bill <= 300) {
-            var tip = 10/100;
-        } else {
-            var tip = 25/100;
+    tipCalc: function () {
+        for (i = 0; i < this.bill.length; i++) {
+            if (this.bill[i] < 100) {
+                var tip = 20/100;
+            } else if (this.bill[i] <= 100 && this.bill[i] <= 300) {
+                var tip = 10/100;
+            } else {
+                var tip = 25/100;
+            }
+            this.tip.push(this.bill[i] * tip);
+            this.payments.push((this.bill[i] * tip) + this.bill[i]);
         }
-        this.tip.push(bill * tip);
-        this.payments.push((bill * tip) + bill);
     }
 }
 
 // TODO: Function to calc the average of the tip
 function Average(array) {
-
     var sum = 0;
     for (i = 0; i < array.length; i++) {
         sum += array[i];
     }
-
     return sum / array.length
 }
 
 // TODO: set their bills
-John.bills = [124, 48, 268];
-Mark.bills = [77, 375, 110, 45];
+John.bill = [124, 48, 268];
+Mark.bill = [77, 375, 110, 45];
 
 // TODO: Call the function to calculate their bills
-for (i = 0; i < John.bills.length; i++) {
-    John.tipCalc(John.bills[i]);
-}
-
-for (i = 0; i < Mark.bills.length; i++) {
-    Mark.tipCalc(Mark.bills[i]);
-}
+John.tipCalc();
+Mark.tipCalc();
 
 // TODO: Call the function to calculate the average
 John.average = Average(John.tip);
